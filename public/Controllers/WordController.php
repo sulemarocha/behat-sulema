b9128c2a2d9413e206f5c40c5364e8285838b902
sulema <sulema.rocha@students.salle.url.edu>
2020-05-25 23:44:49 +0200

    Added endpoint: Random Cool Word localhost/word

--- public/Controllers/WordController.php
+++ public/Controllers/WordController.php
@@ -4,14 +4,23 @@
 namespace LaSallePublic\ChupiProject\Controllers;
 
 
+use LaSalle\ChupiProject\Module\CoolWord\Application\RandomCoolWordSearcher;
+use LaSalle\ChupiProject\Module\CoolWord\Infrastructure\InMemoryCoolWordRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class WordController extends BaseController
 {
+    private $getCoolWord;
+    public function __construct(array $dc)
+    {
+        parent::__construct($dc);
+        $this->getCoolWord = new RandomCoolWordSearcher(new InMemoryCoolWordRepository());
+    }
+
     public function __invoke(Request $request): Response
     {
-        // TODO: Implement __invoke() method.
-        return Response::create('Color', 200);
+        $coolWordGenerated = $this->getCoolWord->__invoke();
+        return Response::create($coolWordGenerated, 200);
     }
 }
