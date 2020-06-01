244c9c23f3a86b70463697adde2f00bcf5a2bd95
sulema <sulema.rocha@students.salle.url.edu>
2020-06-01 18:34:37 +0200

    Test with Behat: use case random Color Searcher

---
+++ features/RandomColorSearcher.feature
@@ -0,0 +1,5 @@
+Feature: Random Color Searcher
+    Scenario: Generate a color random
+        Given I have an array of colors
+        When I generate a color from use case
+        Then The result should be a color
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,9 +1,12 @@
 <?php
 
 use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
-
+use LaSalle\ChupiProject\Module\Color\Application\RandomColorSearcher;
+use LaSalle\ChupiProject\Module\Color\Infrastructure\InMemoryColorRepository;
+use PHPUnit\Framework\Assert;
 /**
  * Defines application features from the specific context.
  */
@@ -16,7 +19,36 @@ class FeatureContext implements Context
      * You can also pass arbitrary arguments to the
      * context constructor through behat.yml.
      */
+    private $colors;
+    private $randomColorSearcher;
+    private $colorGenerated;
     public function __construct()
     {
+        $this->randomColorSearcher = new RandomColorSearcher(new InMemoryColorRepository());
     }
+
+    /**
+     * @Given I have an array of colors
+     */
+    public function iHaveAnArrayOfColors()
+    {
+        $this->colors =  ['red', 'cyan', 'magenta', 'green', 'black', 'yellow', 'blue', 'light_gray'];
+    }
+
+    /**
+     * @When I generate a color from use case
+     */
+    public function iGenerateAColorFromUseCase()
+    {
+        $this->colorGenerated = $this->randomColorSearcher->__invoke();
+    }
+
+    /**
+     * @Then The result should be a color
+     */
+    public function theResultShouldBeAColor()
+    {
+        Assert::assertContains($this->colorGenerated, $this->colors, 'not is a color');
+    }
+
 }
